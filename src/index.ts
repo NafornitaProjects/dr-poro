f6f23e37819b2f7d0dcb073170504be89bd47c63
Edward Nafornita <83386759+edwardnafornita@users.noreply.github.com>
2025-08-08 18:08:47 -0400

    Fixed the issue where the bot will be perpetually stuck on picking the minutes in the time picker (#4)
    Co-authored-by: Edward Nafornita <contact@edwardnafornita.com>

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { handleTimeSelection } from './interactions/timePicker.js';
 import { handleTeamInput } from './interactions/teamInput.js';
 import { handleConfirmation } from './interactions/confirmPost.js';
 import { handleTypeSelection } from './interactions/selectType.js';
+import { handleHalfHourSelection } from './interactions/selectHalfHour.js';
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -26,6 +27,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
     if (interaction.customId === 'select-type') await handleTypeSelection(interaction);
     if (interaction.customId.startsWith('select-date|')) await handleDateSelection(interaction);
     if (interaction.customId.startsWith('select-time|')) await handleTimeSelection(interaction);
+    if (interaction.customId.startsWith('select-half-hour|')) await handleHalfHourSelection(interaction);
   }
 
   if (interaction.isModalSubmit() && interaction.customId.startsWith('team-input|')) {
